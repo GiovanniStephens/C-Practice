0f2f85e80114ec8f8f07c20edef30200867d9e04
Giovanni-Stephens <stephens.giovanni@gmail.com>
2022-03-29 20:05:48 +1300

    Added a comment.

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,3 +1,10 @@
+/* This is a simple program that prints "Hello, World!" to the screen. 
+ * It is a practice program to get familiar with C++.
+ * 
+ * Author: Giovanni Stephens
+ * Date:   28/03/2022
+ */
+
 #include <iostream>
 
 int main(){
