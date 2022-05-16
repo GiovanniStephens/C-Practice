2e9ea7062aafaf46753705591cc84ecf7dec27f0
GiovanniStephens <stephens.giovanni@gmail.com>
2022-05-16 21:57:42 +1200

    Updated the graph to be undirected.

--- graph.cpp
+++ graph.cpp
@@ -1,6 +1,8 @@
 /** This is a basic implementation of a
- * graph data structure. I am still unsure about
- * which type I want to create.
+ * graph data structure. It is undirected,
+ * meaning that there is no distinction between
+ * edges going from A to B and edges going from
+ * B to A.
  * 
  * Author:  Giovanni Stephens
  * Date:    15/05/2022
@@ -62,6 +64,7 @@ class graph {
             node* n1 = this->get_node(name1);
             node* n2 = this->get_node(name2);
             n1->edges.push_back(n2);
+            n2->edges.push_back(n1);
         }
 
 
@@ -91,16 +94,11 @@ int main() {
     g.add_node("A");
     g.add_node("B");
     g.add_node("C");
-    g.add_node("D");
-    g.add_node("E");
-    g.add_node("F");
+
     g.add_edge("A", "B");
     g.add_edge("A", "C");
-    g.add_edge("B", "D");
-    g.add_edge("C", "E");
-    g.add_edge("D", "E");
-    g.add_edge("D", "F");
-    g.add_edge("E", "F");
+    g.add_edge("B", "C");
+
     g.print();
 
     return 0;
