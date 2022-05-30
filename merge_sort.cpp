88acc442b6ca502a56274b05bdd74d6af752854c
GiovanniStephens <stephens.giovanni@gmail.com>
2022-05-30 14:30:20 +1200

    Merge sort.

---
+++ merge_sort.cpp
@@ -0,0 +1,50 @@
+/* Merge sort imlementation. 
+ * This is for practicing c++.
+ * 
+ * Author:  Giovanni Stephens
+ * Date:    30/05/2022
+ */
+
+#include <iostream>
+
+void merge(int *arr, int left, int mid, int right) {
+    int *temp = new int[right - left + 1];
+    int i = left, j = mid + 1, k = 0;
+    while (i <= mid && j <= right) {
+        if (arr[i] < arr[j]) {
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++];
+        }
+    }
+    while (i <= mid) {
+        temp[k++] = arr[i++];
+    }
+    while (j <= right) {
+        temp[k++] = arr[j++];
+    }
+    for (int i = 0; i < k; i++) {
+        arr[left + i] = temp[i];
+    }
+    delete[] temp;
+}
+
+void merge_sort(int *arr, int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+        merge_sort(arr, left, mid);
+        merge_sort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    int arr[] = {5, 2, 4, 6, 1, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    merge_sort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
