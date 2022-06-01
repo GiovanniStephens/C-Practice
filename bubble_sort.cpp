b34a1cd1e7754324a0de470797704a851ff697cf
GiovanniStephens <stephens.giovanni@gmail.com>
2022-06-01 15:28:14 +1200

    Adding a header and removing the main function.

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -4,36 +4,22 @@
  * Date:    28/05/2022
  */
 #include <iostream>
+#include "sorting.h"
 
-void print_array(int *array, int size) {
-    for (int i = 0; i < size; i++) {
-        std::cout << array[i] << " ";
-    }
-    std::cout << std::endl;
-}
-
-int main() {
-    int array[5] = {5, 4, 3, 2, 1};
-    int i, j, temp, swapped;
-
-    print_array(array, 5);
-
-    for (i = 0; i < 5; ++i) {
+void bubble_sort(int *arr, int n) {
+    int swapped;
+    for (int i = 0; i < n - 1; ++i) {
         swapped = 0;
-        for (j = 0; j < 5 - i - 1; ++j) {
-            if (array[j] > array[j + 1]) {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
                 swapped = 1;
             }
         }
-        if (swapped == 0) {
+        if (!swapped) {
             break;
         }
     }
-
-    print_array(array, 5);
-
-    return 0;
 }
