14659ce4d28ec0c557e2aea70fd6d8dc7774a78a
GiovanniStephens <stephens.giovanni@gmail.com>
2022-06-01 15:48:59 +1200

    Fixed the parameters of the merge_sort definition.

--- sorting.h
+++ sorting.h
@@ -6,7 +6,7 @@
 void bubble_sort(int *arr, int n);
 void insertion_sort(int *arr, int n);
 void selection_sort(int *arr, int n);
-void merge_sort(int *arr, int left, int mid, int right);
+void merge_sort(int *arr, int left, int right);
 void quick_sort(int arr[], int low, int high);
 void heap_sort(int *arr, int n, int i);
 void counting_sort(std::vector<int> &arr);
