97630a9f7f4251f6ffd3cecfe272337e65b7e8bc
GiovanniStephens <stephens.giovanni@gmail.com>
2022-06-09 21:27:52 +1200

    Initial commit to create a utilities file for running all the corrollary functions.

---
+++ Regression/utilities.cpp
@@ -0,0 +1,17 @@
+/* Contains functions that are useful for 
+ * fitting an ARMA model.
+ *
+ * Author:  Giovanni Stephens
+ * Date:    09/06/2022
+ */
+
+#include <iostream>
+#include <vector>
+
+std::vector<float> calculate_returns(std::vector<float> prices) {
+    std::vector<float> returns;
+    for (int i = 1; i < prices.size(); i++) {
+        returns.push_back((prices[i] - prices[i-1]) / prices[i-1]);
+    }
+    return returns;
+}
