1817e0b48e220304929a1e7caa688e9d1041fc71
GiovanniStephens <stephens.giovanni@gmail.com>
2022-06-18 22:50:00 +1200

    Adding a second matrix inverting definition.

--- Regression/Regressor.h
+++ Regression/Regressor.h
@@ -10,10 +10,12 @@ public:
     Matrix cofactor(unsigned int p, unsigned int q);
     float determinant();
     Matrix inverse();
+    Matrix inverse_2();
     Matrix transpose();
     Matrix multiply(Matrix m);
     void print();
     unsigned int n;
+    unsigned int n_cols;
 private:
     std::vector<std::vector<float>> matrix;
 };
@@ -25,7 +27,6 @@ class LinearRegressor {
     public:
         std::vector<float> coefficients;
         LinearRegressor(std::vector<std::vector<float>> x, std::vector<float> y);
-        ~LinearRegressor();
         void fit();
         float predict(std::vector<float> x);
 };
