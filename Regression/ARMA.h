1a7d2cfe98d6d8e85c00f08128b4a8a4f6d26f3c
GiovanniStephens <stephens.giovanni@gmail.com>
2022-06-21 20:24:39 +1200

    Adding the skeleton for the ARMA class.

--- Regression/ARMA.h
+++ Regression/ARMA.h
@@ -4,4 +4,19 @@
 #include "Regressor.h"
 #include "utilities.h"
 
+class ARMA {
+    private:
+        int p;
+        int q;
+        std::vector<std::vector<float>> data;
+    public:    
+        std::vector<float> residuals;
+        std::vector<float> coeffients;
+
+        ARMA(std::vector<std::vector<float>> data, int p, int q);
+        ~ARMA();
+        void fit();
+        void predict(int n);
+};
+
 #endif
