1f407cd0df3811a8b42bf31163ce05d98388370b
Giovanni Stephens <stephens.giovanni@gmail.com>
2022-09-09 20:10:06 +1200

    Adding the outline for some crossover functions.

--- evolutionary_algorithm.cpp
+++ evolutionary_algorithm.cpp
@@ -16,35 +16,46 @@ class EA {
     bool elitism;
     vector<vector<int>> population;
 
-    public:
-        EA(int population_size,
-           int generations,
-           float crossover_probability,
-           float mutation_probability,
-           bool elitism) {
-            this->population_size = population_size;
-            this->generations = generations;
-            this->crossover_probability = crossover_probability;
-            this->mutation_probability = mutation_probability;
-            this->elitism = elitism;
-           }
+    void crossover(vector<int> *parent_1, vector<int> *parent_2,
+                   vector<int> *child) {
+        /* Does a random split between two individials.*/
 
-        void create_individual(int len){
-            /* Creates a random binary vector 
-             * to represent a random gene. 
-             *
-             * Todo: Paralellize this.
-             */
-            vector<int> individual;
-            for (int i = 0; i < len; i++){
-                individual.push_back(rand() % 2);
-            }
-            population.push_back(individual);
+        // Pick a random splitting point
+        // Update the child with values from parent_1
+        // on the left of the splitting point,
+        // and parent_2 on the right.
+    }
+
+    void random_crossover(vector<int> *parent_1, vector<int> *parent_2,
+                          vector<int> *child) {
+        /* Does a random crossover between two individials.*/
+
+        // for each element in the child, randomly pick
+        // from either of the parents.
+    }
+
+  public:
+    EA(int population_size, int generations, float crossover_probability,
+       float mutation_probability, bool elitism) {
+        this->population_size = population_size;
+        this->generations = generations;
+        this->crossover_probability = crossover_probability;
+        this->mutation_probability = mutation_probability;
+        this->elitism = elitism;
+    }
+
+    void create_individual(int len) {
+        /* Creates a random binary vector
+         * to represent a random gene.
+         *
+         * Todo: Paralellize this.
+         */
+        vector<int> individual;
+        for (int i = 0; i < len; i++) {
+            individual.push_back(rand() % 2);
         }
+        population.push_back(individual);
+    }
 };
 
-
-int main() {
-
-    return 1;
-}
+int main() { return 1; }
